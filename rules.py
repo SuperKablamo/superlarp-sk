8fe4adfaa500c681fa3d122f363d4cb5d1477a18
Will Merydith <will.merydith@gmail.com>
2011-04-01 22:35:11 -0500

    rollEncounter bug fixes.

--- rules.py
+++ rules.py
@@ -22,6 +22,7 @@ from settings import *
 import logging
 import time
 
+from google.appengine.ext import db
 from random import randrange
 
 ######################## METHODS #############################################
@@ -44,6 +45,7 @@ def rollEncounter(player_party, geo_pt):
     monsters or None.  The chance of encountering monsters is based on the
     player_party's encounter log.
     """ 
+    logging.info(TRACE+'rollEncounter()')
     # Determine likelyhood of encounter ...
     '''
     {'encounters': {'total': 23, 'uniques': 2, 'start_time': POSIX
@@ -66,19 +68,22 @@ def rollEncounter(player_party, geo_pt):
         player_party.log['encounters']['last_encounter'] = last_encounter
         
         monster_party = models.NonPlayerParty(location = geo_pt, 
-                                              monsters = None,
+                                              monsters = [],
                                               json = {'monsters': []})
         
         # Get number of PCs and the average level of the Party ...
         party_size = len(player_party.members)
+        members = db.get(player_party.members)
         total = 0
-        for m in player_party.members:
+        for m in members:
             total = total + m.level
         avg_level = total/party_size
+        logging.info(TRACE+'rollEncounter():: avg_level = '+str(avg_level))
 
         # Get the appropriate Monster table for the Party level ...    
-        monster_level = MONSTER_XP_LEVEL[avg_level]
-
+        monster_level = MONSTER_XP_LEVELS[avg_level]
+        logging.info(TRACE+'rollEncounter():: monster_level = '+str(monster_level))
+        
         # Get the XP total for this Encounter
         xp_total = monster_level[models.STAN]*party_size    
         
@@ -93,7 +98,7 @@ def rollEncounter(player_party, geo_pt):
         # Minions Minions Minions!
         if r == 1:
             logging.info(TRACE+'rollEncounter():: Minions Minions Minions!')              
-            q = Query('NonPlayerCharacter', key_only=True)
+            q = db.Query('NonPlayerCharacter', key_only=True)
             q.filter('role =', models.MIN)
             q.filter('challenge = ', models.STAN)
             q.filter('level = ', avg_level)
@@ -113,7 +118,7 @@ def rollEncounter(player_party, geo_pt):
         # Solo boss - uh-oh.                        
         elif r == 2:  
             logging.info(TRACE+'rollEncounter():: Solo boss - uh-oh!')              
-            q = Query('NonPlayerCharacter', key_only=True)
+            q = db.Query('NonPlayerCharacter', key_only=True)
             q.filter('challenge = ', models.SOLO)
             q.filter('level = ', avg_level)
             q.filter('unique =', False)            
@@ -131,7 +136,7 @@ def rollEncounter(player_party, geo_pt):
         elif r == 3:
             logging.info(TRACE+'rollEncounter():: Minions + Mini-boss!')                
             # Get Minions
-            q = Query('NonPlayerCharacter', key_only=True)
+            q = db.Query('NonPlayerCharacter', key_only=True)
             q.filter('role =', models.MIN)
             q.filter('challenge = ', models.STAN)
             q.filter('level = ', avg_level)
@@ -150,7 +155,7 @@ def rollEncounter(player_party, geo_pt):
                 entities.append(minion)
             
             # Get Mini-boss
-            q = Query('NonPlayerCharacter', key_only=True)
+            q = db.Query('NonPlayerCharacter', key_only=True)
             q.filter('role !=', models.MIN)
             q.filter('challenge = ', models.ELIT)
             q.filter('level = ', avg_level)
@@ -182,7 +187,7 @@ def rollEncounter(player_party, geo_pt):
                 avg_level -= 2
                     
             # Get Minions
-            q = Query('NonPlayerCharacter', key_only=True)
+            q = db.Query('NonPlayerCharacter', key_only=True)
             q.filter('role =', models.MIN)
             q.filter('challenge = ', models.STAN)
             q.filter('level = ', avg_level)
@@ -202,7 +207,7 @@ def rollEncounter(player_party, geo_pt):
             
             # Get Standards if the Player Party level is high enough            
             if not avg_lvl < 3:
-                q = Query('NonPlayerCharacter', key_only=True)
+                q = db.Query('NonPlayerCharacter', key_only=True)
                 q.filter('role !=', models.MIN)
                 q.filter('challenge = ', models.STAN)
                 q.filter('level = ', avg_level)
@@ -224,7 +229,7 @@ def rollEncounter(player_party, geo_pt):
         elif r == 6: # Unique NPC.
             logging.info(TRACE+'rollEncounter():: Unique NPC')
             player_party.log['encounters']['uniques'] += 1            
-            q = Query('NonPlayerCharacter', key_only=True)
+            q = db.Query('NonPlayerCharacter', key_only=True)
             q.filter('level = ', avg_level)
             q.filter('unique =', True)            
             npc_keys = q.fetch(100)
@@ -238,7 +243,7 @@ def rollEncounter(player_party, geo_pt):
             
             # More than 1 Player? Throw in some Minions and make it a Party!
             if party_size > 1:
-                q = Query('NonPlayerCharacter', key_only=True)
+                q = db.Query('NonPlayerCharacter', key_only=True)
                 q.filter('role =', models.MIN)
                 q.filter('challenge = ', models.STAN)
                 q.filter('level = ', avg_level)
@@ -277,32 +282,32 @@ def logBattle(location, loot, *characters):
 ##############################################################################    
 
 MONSTER_XP_LEVELS = [
-    {'1': {models.STAN: 100, models.MIN: 25, models.ELIT: 200, models.SOLO: 500} },
-    {'2': {models.STAN: 125, models.MIN: 31, models.ELIT: 250, models.SOLO: 625} },
-    {'3': {models.STAN: 150, models.MIN: 38, models.ELIT: 300, models.SOLO: 750} },
-    {'4': {models.STAN: 175, models.MIN: 44, models.ELIT: 350, models.SOLO: 875} },
-    {'5': {models.STAN: 200, models.MIN: 50, models.ELIT: 400, models.SOLO: 1000} },
-    {'6': {models.STAN: 250, models.MIN: 63, models.ELIT: 500, models.SOLO: 1250} },
-    {'7': {models.STAN: 300, models.MIN: 75, models.ELIT: 600, models.SOLO: 1500} },
-    {'8': {models.STAN: 350, models.MIN: 88, models.ELIT: 700, models.SOLO: 1750} },
-    {'9': {models.STAN: 400, models.MIN: 100, models.ELIT: 800, models.SOLO: 2000} },
-    {'10': {models.STAN: 500, models.MIN: 125, models.ELIT: 1000, models.SOLO: 2500} },
-    {'11': {models.STAN: 600, models.MIN: 150, models.ELIT: 1200, models.SOLO: 3000} },
-    {'12': {models.STAN: 700, models.MIN: 175, models.ELIT: 1400, models.SOLO: 3500} },
-    {'13': {models.STAN: 800, models.MIN: 200, models.ELIT: 1600, models.SOLO: 4000} },
-    {'14': {models.STAN: 1000, models.MIN: 250, models.ELIT: 2000, models.SOLO: 5000} },
-    {'15': {models.STAN: 1200, models.MIN: 300, models.ELIT: 2400, models.SOLO: 6000} },
-    {'16': {models.STAN: 1400, models.MIN: 350, models.ELIT: 2800, models.SOLO: 7000} },
-    {'17': {models.STAN: 1600, models.MIN: 400, models.ELIT: 3200, models.SOLO: 8000} },
-    {'18': {models.STAN: 2000, models.MIN: 500, models.ELIT: 4000, models.SOLO: 10000} },
-    {'19': {models.STAN: 2400, models.MIN: 600, models.ELIT: 4800, models.SOLO: 12000} },
-    {'20': {models.STAN: 2800, models.MIN: 700, models.ELIT: 5600, models.SOLO: 14000} },
-    {'21': {models.STAN: 3200, models.MIN: 800, models.ELIT: 6400, models.SOLO: 16000} },
-    {'22': {models.STAN: 4150, models.MIN: 1038, models.ELIT: 8300, models.SOLO: 20750} },
-    {'23': {models.STAN: 5100, models.MIN: 1275, models.ELIT: 10200, models.SOLO: 25500} },
-    {'24': {models.STAN: 6050, models.MIN: 1513, models.ELIT: 12100, models.SOLO: 30250} },
-    {'25': {models.STAN: 7000, models.MIN: 1750, models.ELIT: 14000, models.SOLO: 35000} },
-    {'26': {models.STAN: 9000, models.MIN: 2250, models.ELIT: 18000, models.SOLO: 45000} },
-    {'27': {models.STAN: 11000, models.MIN: 2750, models.ELIT: 22000, models.SOLO: 55000} },
-    {'28': {models.STAN: 13000, models.MIN: 3250, models.ELIT: 26000, models.SOLO: 65000} }]
+    {models.STAN: 100, models.MIN: 25, models.ELIT: 200, models.SOLO: 500},
+    {models.STAN: 125, models.MIN: 31, models.ELIT: 250, models.SOLO: 625},
+    {models.STAN: 150, models.MIN: 38, models.ELIT: 300, models.SOLO: 750},
+    {models.STAN: 175, models.MIN: 44, models.ELIT: 350, models.SOLO: 875},
+    {models.STAN: 200, models.MIN: 50, models.ELIT: 400, models.SOLO: 1000},
+    {models.STAN: 250, models.MIN: 63, models.ELIT: 500, models.SOLO: 1250},
+    {models.STAN: 300, models.MIN: 75, models.ELIT: 600, models.SOLO: 1500},
+    {models.STAN: 350, models.MIN: 88, models.ELIT: 700, models.SOLO: 1750},
+    {models.STAN: 400, models.MIN: 100, models.ELIT: 800, models.SOLO: 2000},
+    {models.STAN: 500, models.MIN: 125, models.ELIT: 1000, models.SOLO: 2500},
+    {models.STAN: 600, models.MIN: 150, models.ELIT: 1200, models.SOLO: 3000},
+    {models.STAN: 700, models.MIN: 175, models.ELIT: 1400, models.SOLO: 3500},
+    {models.STAN: 800, models.MIN: 200, models.ELIT: 1600, models.SOLO: 4000},
+    {models.STAN: 1000, models.MIN: 250, models.ELIT: 2000, models.SOLO: 5000},
+    {models.STAN: 1200, models.MIN: 300, models.ELIT: 2400, models.SOLO: 6000},
+    {models.STAN: 1400, models.MIN: 350, models.ELIT: 2800, models.SOLO: 7000},
+    {models.STAN: 1600, models.MIN: 400, models.ELIT: 3200, models.SOLO: 8000},
+    {models.STAN: 2000, models.MIN: 500, models.ELIT: 4000, models.SOLO: 10000},
+    {models.STAN: 2400, models.MIN: 600, models.ELIT: 4800, models.SOLO: 12000},
+    {models.STAN: 2800, models.MIN: 700, models.ELIT: 5600, models.SOLO: 14000},
+    {models.STAN: 3200, models.MIN: 800, models.ELIT: 6400, models.SOLO: 16000},
+    {models.STAN: 4150, models.MIN: 1038, models.ELIT: 8300, models.SOLO: 20750},
+    {models.STAN: 5100, models.MIN: 1275, models.ELIT: 10200, models.SOLO: 25500},
+    {models.STAN: 6050, models.MIN: 1513, models.ELIT: 12100, models.SOLO: 30250},
+    {models.STAN: 7000, models.MIN: 1750, models.ELIT: 14000, models.SOLO: 35000},
+    {models.STAN: 9000, models.MIN: 2250, models.ELIT: 18000, models.SOLO: 45000},
+    {models.STAN: 11000, models.MIN: 2750, models.ELIT: 22000, models.SOLO: 55000},
+    {models.STAN: 13000, models.MIN: 3250, models.ELIT: 26000, models.SOLO: 65000}]
 
